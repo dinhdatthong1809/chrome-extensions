c99e6a746a1e7c6fd92419d30b8ba47b64f2520f
BlueShootingStar <BlueShootingStar1809@gmail.com>
2020-02-17 14:28:55 +0700

    nothing

--- GPXPlus-tricker/assets/js/execute/searchEgg.js
+++ GPXPlus-tricker/assets/js/execute/searchEgg.js
@@ -1,11 +1,11 @@
 searchEgg();
 
 function searchEgg() {
+    $(`img[data-tooltip='Mystery Egg']`).click();
+    $("button[data-value='1']").click();
+
     for (let i = 0; i < eggNames.length; i++) {
         let eggName = eggNames[i] + " Egg";
-
-        $(`img[data-tooltip='Mystery Egg']`).click();
-        $("button[data-value='1']").click();
 
         $(`img[data-tooltip='${eggName}']`).click();
         $("button[data-value='1']").click();
